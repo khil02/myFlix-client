db355713a4075eebf6054ae9ab7a57d85fa77e64
khil02 <120340087+khil02@users.noreply.github.com>
2023-08-25 17:31:49 -0400

    pulling data from API, unknown issue
    data pulls from api but will not display for some reason

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,6 @@
-export const MovieCard =({ movie, onMovieClick }) => {
+import PropTypes from "prop-types";
+
+export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
             onClick={() => {
@@ -8,4 +10,30 @@ export const MovieCard =({ movie, onMovieClick }) => {
         {movie.title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+            //_id: PropTypes.string.isRequired,
+            Title: PropTypes.string.isRequired,
+            ImagePath: PropTypes.string.isRequired,
+            Description: PropTypes.string.isRequired,
+
+            // Director: PropTypes.shape({
+            //     Name: PropTypes.string.isRequired,
+            //     Bio: PropTypes.string.isRequired,
+            //     // birth string is probably incorrect
+            //     Birth: PropTypes.string.isRequired
+            // }).isRequired,
+
+            // Genre: PropTypes.shape({
+            //     Name: PropTypes.string.isRequired,
+            //     Description: PropTypes.string.isRequired,
+            // }).isRequired,
+            //actors: PropTypes.shape({ })
+            //_id: PropTypes.string.isRequired,
+            Featured: PropTypes.bool.isRequired
+            
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired
 };
